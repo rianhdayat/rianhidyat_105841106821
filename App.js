ca3ccded10212484143da232f2ebcfdbce4c7f62
rianhdyat <105841106821@student.unismuh.ac.id>
2024-06-28 14:31:04 +0800

    useFount

--- App.js
+++ App.js
@@ -1,73 +1,26 @@
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
-import React from "react";
+// Rest of the import statements
+import { useFonts } from 'expo-font';
+import { Text, View } from 'react-native';
 
-
-const App = () => {
-   
-  const TextInputCustom = ({name, color}) => {
-    return (
-      <TextInput
-      placeholder={`Masukan ${name}`}
-      style={{
-        width: 300,
-        height: 40,
-        borderWidth: 1,
-        borderColor: color,
-        paddingLeft: 10,
-        marginBottom: 10,
-        margin: 10,
-      }}/>
-    )
+export default function App() {
+  const [dapatFont] = useFonts({
+    'MetroBlack': require('./assets/fonts/Metropolis-Black.otf'),
+    'MetroBold': require('./assets/fonts/Metropolis-Bold.otf'),
+    'MetroLight': require('./assets/fonts/Metropolis-Light.otf'),
+    'MetroMedium': require('./assets/fonts/Metropolis-Medium.otf'),
+    'MetroSemibold': require('./assets/fonts/Metropolis-SemiBold.otf'),
+  });
+  if (!dapatFont) {
+    return <Text>Font Tidak Ditemukan...</Text>
   }
- const ButtonCustom = ({text, color}) => {
   return (
-    <View style={{
-      width: 300,
-      height: 30,
-      backgroundColor: color,
-      justifyContent: 'center',
-      borderRadius: 20,
-      marginLeft: 10,
-      alignSelf: 'center',
-      alignItems: 'center',
-      marginBottom: 10,
-    }}>
-      <Text style={{
-        textAlign: 'flex-start',
-        color: 'white',
-        fontsize: 12,
-      }}>{text}
-      </Text>  
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontFamily: 'MetroBlack', fontSize: 24}}>Font Metropolis Black</Text>
+      <Text style={{fontFamily: 'MetroBold', fontSize: 24}}>Font Metropolis Bold</Text>
+      <Text style={{fontFamily: 'MetroLight', fontSize: 24}}>Font Metropolis Light</Text>
+      <Text style={{fontFamily: 'MetroMedium', fontSize: 24}}>Font Metropolis Medium</Text>
+      <Text style={{fontFamily: 'MetroSemibold', fontSize: 24}}>Font Metropolis SemiBold</Text>
+      <Text>Font Biasa</Text>
     </View>
   )
- }
- return (
-  <View>
-   <Text style={{
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    marginTop: 70,
-    marginLeft: 10,
-    
-   }}>
-      Forgot Password?
-   </Text>
-   <Text style={{
-      fontSize: 14,
-      lineHeight: '20px',
-      marginLeft: 10,
-      marginBottom: 10,
-      fontFamily: 'metropolis',
-    }}>
-      Please enter your email address. You will receive <br/>
-      a link to create a new password via email. 
-   </Text>
-    <TextInputCustom name="Email" color="red"/>
-    <ButtonCustom text="Send" color="red"/>
-  </View>
- )
-
 }
-
-export default  App;
